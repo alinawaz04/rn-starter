02ee923088b3318b36e69ab8b280f299c7ffb87f
alinawaz04 <alihnawaz@gmail.com>
2024-05-14 15:12:00 -0500

    rules of jsx

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -1,8 +1,22 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View } from "react-native";
 
 const ComponentsScreen = () => {
-  return <Text style={styles.textStyle}>OutlineOutlineOutline</Text>;
+  // var showed inside of JSX can be anything but JS object
+  const greeting = "Hi there";
+  // can assign JSX els to var, then show that var inside of JSX block
+  const greetingElement = <Text>Hello to you</Text>;
+
+  return (
+    // can assemble different JSX elements like normal HTML
+    <View>
+      {/* style is built in prop of Text */}
+      <Text style={styles.textStyle}>OutlineOutlineOutline</Text>
+      {/* can refer to js vars inside of JSX block by using curly braces */}
+      <Text>{greeting}</Text>
+      {greetingElement}
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
