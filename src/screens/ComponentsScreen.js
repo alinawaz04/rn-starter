802e6415788479cecedcc24255969c0d977e55b0
alinawaz04 <alihnawaz@gmail.com>
2024-05-14 15:16:41 -0500

    JSX exercise

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -2,26 +2,22 @@ import React from "react";
 import { Text, StyleSheet, View } from "react-native";
 
 const ComponentsScreen = () => {
-  // var showed inside of JSX can be anything but JS object
-  const greeting = "Hi there";
-  // can assign JSX els to var, then show that var inside of JSX block
-  const greetingElement = <Text>Hello to you</Text>;
+  const name = "Ali";
 
   return (
-    // can assemble different JSX elements like normal HTML
     <View>
-      {/* style is built in prop of Text */}
-      <Text style={styles.textStyle}>OutlineOutlineOutline</Text>
-      {/* can refer to js vars inside of JSX block by using curly braces */}
-      <Text>{greeting}</Text>
-      {greetingElement}
+      <Text style={styles.textStyle}>Getting Started with React Native!</Text>
+      <Text style={styles.subHeaderStyle}>My name is {name}</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   textStyle: {
-    fontSize: 30,
+    fontSize: 45,
+  },
+  subHeaderStyle: {
+    fontSize: 20,
   },
 });
 
