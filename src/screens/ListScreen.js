26b97b382ac7507f89c4f775c43b19965470d241
alinawaz04 <alihnawaz@gmail.com>
2024-05-15 10:47:29 -0500

    flatlist notes

---
+++ src/screens/ListScreen.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+/*
+lists - we usually begin with an array of objects
+- we will utilize the FlatList primitive React Native element
+- we will pass is two props - data (array of data) and renderItem prop (turns each individual item into an element)
+- this is the react native equivalent to mapping array in react on web.
+    - FlatList is well optimized specifically for mobile
+*/
+const ListScreen = () => {
+  return <Text>List Screen</Text>;
+};
+
+const styles = StyleSheet.create({});
+
+export default ListScreen;
