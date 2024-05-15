602bb015a56c584f069fa18162b31c3455108651
alinawaz04 <alihnawaz@gmail.com>
2024-05-15 15:36:52 -0500

    section and exercise (passing new prop and rendering) complete

--- src/components/ImageDetail.js
+++ src/components/ImageDetail.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Text, View, StyleSheet, Image } from "react-native";
 
-const ImageDetail = ({ title, image }) => {
+const ImageDetail = ({ title, image, score }) => {
   console.log(image);
   return (
     <View>
       <Image source={image} />
       <Text>{title}</Text>
+      <Text>Image score - {score}</Text>
     </View>
   );
 };
--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -5,11 +5,20 @@ import ImageDetail from "../components/ImageDetail";
 const ImageScreen = () => {
   return (
     <View>
-      <ImageDetail image={require(`../../assets/forest.jpg`)} title="Forest" />
-      <ImageDetail image={require(`../../assets/beach.jpg`)} title="Beach" />
+      <ImageDetail
+        image={require(`../../assets/forest.jpg`)}
+        title="Forest"
+        score={10}
+      />
+      <ImageDetail
+        image={require(`../../assets/beach.jpg`)}
+        title="Beach"
+        score={8}
+      />
       <ImageDetail
         image={require(`../../assets/mountain.jpg`)}
         title="Mountain"
+        score={6}
       />
     </View>
   );
