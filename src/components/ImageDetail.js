5da176591110fd41d9d3758d8c9a24c271394824
alinawaz04 <alihnawaz@gmail.com>
2024-05-15 15:21:39 -0500

    rendering a component multiple times on a screen

---
+++ src/components/ImageDetail.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+
+const ImageDetail = () => {
+  return <Text>Image Detail</Text>;
+};
+
+const styles = StyleSheet.create({});
+
+export default ImageDetail;
