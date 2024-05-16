ca5c1aa6149dc6a4f96691ff37848c82853e4ea3
alinawaz04 <alihnawaz@gmail.com>
2024-05-16 12:16:27 -0500

    counter state lifecycle notes

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,6 +1,19 @@
 import React, { useState } from "react";
 import { Text, View, StyleSheet, Button } from "react-native";
 
+/* APP/STATE LIFECYCLE
+
+- App starts up
+- We navigate to CounterScreen. `counter` piece of state is init to default (0)
+- User taps on "increase" button, onPress callback runs
+- "onPress" calls "setCounter", and updates its value
+- After a brief pause - react automatically rerenders or reruns the CounterScreen
+- CounterScreen was already ran once, so the state of `counter` is not initialized again
+- `counter` now has an updated value of 1
+- We return some JSX that references the updated `counter` variable
+
+*/
+
 const CounterScreen = () => {
   const [counter, setCounter] = useState(0);
   return (
