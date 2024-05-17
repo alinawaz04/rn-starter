d3440136b5294012c0d0e5476385b3c51894dd10
alinawaz04 <alihnawaz@gmail.com>
2024-05-17 11:00:30 -0500

    changing reducer to follow community conventions

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -6,25 +6,26 @@ const COLOR_INCREMENT = 15;
 
 const reducer = (state, action) => {
   // state === { red: number, green: number, blue: number}
-  // action === { colorToChange: "red" || "green" || "blue", amount 15 || -15 }
+  // action === { type: "change_red" || "change_green" || "change_blue", payload 15 || -15 }
 
-  switch (action.colorToChange) {
-    case "red":
+  switch (action.type) {
+    case "change_red":
       // never do:
       // state.red = state.red - 15
-      return state.red + action.amount > 255 || state.red + action.amount < 0
+      return state.red + action.payload > 255 || state.red + action.payload < 0
         ? state
-        : { ...state, red: state.red + action.amount };
+        : { ...state, red: state.red + action.payload };
 
-    case "green":
-      return state.green + action.amount > 255 ||
-        state.green + action.amount < 0
+    case "change_green":
+      return state.green + action.payload > 255 ||
+        state.green + action.payload < 0
         ? state
-        : { ...state, green: state.green + action.amount };
-    case "blue":
-      return state.blue + action.amount > 255 || state.blue + action.amount < 0
+        : { ...state, green: state.green + action.payload };
+    case "change_blue":
+      return state.blue + action.payload > 255 ||
+        state.blue + action.payload < 0
         ? state
-        : { ...state, blue: state.blue + action.amount };
+        : { ...state, blue: state.blue + action.payload };
     default:
       return state;
   }
@@ -67,10 +68,10 @@ const SquareScreen = () => {
         // onIncrease={() => setColor("red", COLOR_INCREMENT)}
         // onDecrease={() => setColor("red", -1 * COLOR_INCREMENT)}
         onIncrease={() => {
-          dispatch({ colorToChange: "red", amount: COLOR_INCREMENT });
+          dispatch({ type: "change_red", payload: COLOR_INCREMENT });
         }}
         onDecrease={() => {
-          dispatch({ colorToChange: "red", amount: -1 * COLOR_INCREMENT });
+          dispatch({ type: "change_red", payload: -1 * COLOR_INCREMENT });
         }}
         colorVal={state.red}
       />
@@ -79,10 +80,10 @@ const SquareScreen = () => {
         // onIncrease={() => setColor("green", COLOR_INCREMENT)}
         // onDecrease={() => setColor("green", -1 * COLOR_INCREMENT)}
         onIncrease={() => {
-          dispatch({ colorToChange: "green", amount: COLOR_INCREMENT });
+          dispatch({ type: "change_green", payload: COLOR_INCREMENT });
         }}
         onDecrease={() => {
-          dispatch({ colorToChange: "green", amount: -1 * COLOR_INCREMENT });
+          dispatch({ type: "change_green", payload: -1 * COLOR_INCREMENT });
         }}
         colorVal={state.green}
       />
@@ -91,10 +92,10 @@ const SquareScreen = () => {
         // onIncrease={() => setColor("blue", COLOR_INCREMENT)}
         // onDecrease={() => setColor("blue", -1 * COLOR_INCREMENT)}
         onIncrease={() => {
-          dispatch({ colorToChange: "blue", amount: COLOR_INCREMENT });
+          dispatch({ type: "change_blue", payload: COLOR_INCREMENT });
         }}
         onDecrease={() => {
-          dispatch({ colorToChange: "blue", amount: -1 * COLOR_INCREMENT });
+          dispatch({ type: "change_blue", payload: -1 * COLOR_INCREMENT });
         }}
         colorVal={state.blue}
       />
