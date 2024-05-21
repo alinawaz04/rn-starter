c0c328f96bdf844cdd2fda8da750b9f82c9d7e78
alinawaz04 <alihnawaz@gmail.com>
2024-05-21 16:38:03 -0500

    assembling navigator

--- food/App.js
+++ food/App.js
@@ -1,25 +1,19 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import SearchScreen from "./src/screens/SearchScreen";
+
+const navigator = createStackNavigator(
+  { Search: SearchScreen },
+  {
+    initialRouteName: "Search",
+    defaultNavigationOptions: {
+      title: "Business Search",
+    },
+  }
+);
 
 const CLIENT_ID = "bAuTDnGXLPc4bGQjcRiy8Q";
-
 const API_KEY =
   "TS1PFrM2EkDwnaqglq_MNQX-VncuplnpfQKM0My2A6Djo5VbzsMgKzbbZC4Ai-y985OxuLZeNE5s5Vs8MgdLR5zjgkcXmcIZlxQ4S6526bSkQZPZ0JFGQoDkugZNZnYx";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+export default createAppContainer(navigator);
