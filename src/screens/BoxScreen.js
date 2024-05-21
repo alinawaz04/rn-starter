35342e7cb4b18d16796bdbd5272adecbccb61fe1
alinawaz04 <alihnawaz@gmail.com>
2024-05-21 15:20:44 -0500

    layout systems exercise + section complete

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -3,34 +3,37 @@ import { Text, View, StyleSheet } from "react-native";
 
 const BoxScreen = () => {
   return (
-    <View style={styles.viewStyle}>
-      <Text style={styles.textOneStyle}>Child #1</Text>
-      <Text style={styles.textTwoStyle}>Child #2</Text>
-      <Text style={styles.textThreeStyle}>Child #3</Text>
+    <View style={styles.parentViewStyle}>
+      <View style={styles.viewOneStyle} />
+      <View style={styles.viewTwoStyle} />
+      <View style={styles.viewThreeStyle} />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  viewStyle: {
+  parentViewStyle: {
     borderWidth: 3,
     borderColor: "black",
     height: 200,
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
-  textOneStyle: {
-    borderWidth: 3,
-    borderColor: "red",
-    flex: 4, // 40% of available space
+  viewOneStyle: {
+    height: 50,
+    width: 50,
+    backgroundColor: "red",
   },
-  textTwoStyle: {
-    borderWidth: 3,
-    borderColor: "red",
-    flex: 4, // 40% of available space
+  viewTwoStyle: {
+    height: 50,
+    width: 50,
+    backgroundColor: "green",
+    marginTop: 50,
   },
-  textThreeStyle: {
-    borderWidth: 3,
-    borderColor: "red",
-    flex: 2, // 20% of available space
+  viewThreeStyle: {
+    height: 50,
+    width: 50,
+    backgroundColor: "purple",
   },
 });
 
