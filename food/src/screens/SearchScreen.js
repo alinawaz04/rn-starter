abd96d5f30581dfb24ad29d532d9aa6e9e1ab383
alinawaz04 <alihnawaz@gmail.com>
2024-05-22 14:10:49 -0500

    incorrect way of running initial search

--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -6,22 +6,37 @@ import yelp from "../api/yelp";
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
   const [results, setResults] = useState([]);
+  const [error, setError] = useState("");
 
-  const searchApi = async () => {
-    const res = await yelp.get("/search", {
-      params: {
-        limit: 50,
-        term,
-        location: "norman",
-      },
-    });
-    setResults(res.data.businesses);
+  const searchApi = async (searchTerm) => {
+    console.log("test");
+    try {
+      const res = await yelp.get("/search", {
+        params: {
+          limit: 50,
+          term: searchTerm,
+          location: "norman",
+        },
+      });
+      setResults(res.data.businesses);
+    } catch (err) {
+      setError("Something went wrong");
+    }
   };
+
+  // bad code
+  // call searchApi when component is first rendered
+  // searchApi("pasta");
+  // causes an infinite loop of api calls
 
   return (
     <View style={styles.background}>
-      <SearchBar term={term} onTermChange={setTerm} onTermSubmit={searchApi} />
-      <Text>Search Screen</Text>
+      <SearchBar
+        term={term}
+        onTermChange={setTerm}
+        onTermSubmit={() => searchApi(term)}
+      />
+      {error ? <Text>{error}</Text> : null}
       <Text>Found {results.length} results</Text>
     </View>
   );
