60ced710eee4fe0e66c6775eb964381cff59622d
alinawaz04 <alihnawaz@gmail.com>
2024-05-22 15:44:33 -0500

    flatlist rendering + section complete

--- food/src/components/Results.js
+++ food/src/components/Results.js
@@ -1,11 +1,18 @@
 import React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, FlatList } from "react-native";
 
 const Results = ({ title, results }) => {
   return (
     <View>
       <Text style={styles.title}>{title}</Text>
-      <Text>Results: {results.length}</Text>
+      <FlatList
+        horizontal
+        data={results}
+        keyExtractor={(result) => result.id}
+        renderItem={({ item }) => {
+          return <Text>{item.name}</Text>;
+        }}
+      />
     </View>
   );
 };
