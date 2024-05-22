03e276bf8f78b18a7644a87b6498b23255674288
alinawaz04 <alihnawaz@gmail.com>
2024-05-21 21:41:20 -0500

    displaying icons

--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -1,14 +1,21 @@
 import React from "react";
 import { Text, View, StyleSheet } from "react-native";
+import SearchBar from "../components/SearchBar";
 
 const SearchScreen = () => {
   return (
-    <View>
+    <View style={styles.background}>
+      <SearchBar />
       <Text>SearchScreen</Text>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  background: {
+    backgroundColor: "#fff",
+    flex: 1,
+  },
+});
 
 export default SearchScreen;
