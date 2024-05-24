0ada529b0137885c04a721712cab66fa99da5b23
alinawaz04 <alihnawaz@gmail.com>
2024-05-23 19:03:00 -0500

    intro to context + wiring context to app

---
+++ blog/src/context/BlogContext.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+/* CONTEXT
+- moves info from a parent to some nested child
+- complicated to setup, lots of special terms
+- easy to communicate data from a parent to super nested child
+*/
+const BlogContext = React.createContext();
+
+export const BlogProvider = ({ children }) => {
+  return <BlogContext.Provider>{children}</BlogContext.Provider>;
+};
