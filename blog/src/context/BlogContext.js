37595eba4c78a376bd4be229e47d66cc77cf6daa
alinawaz04 <alihnawaz@gmail.com>
2024-05-23 19:10:28 -0500

    moving data with context

--- blog/src/context/BlogContext.js
+++ blog/src/context/BlogContext.js
@@ -8,5 +8,9 @@ import React from "react";
 const BlogContext = React.createContext();
 
 export const BlogProvider = ({ children }) => {
-  return <BlogContext.Provider>{children}</BlogContext.Provider>;
+  return (
+    <BlogContext.Provider value={"hello"}>{children}</BlogContext.Provider>
+  );
 };
+
+export default BlogContext;
