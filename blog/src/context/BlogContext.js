92884bbce4cd11bfaa5173331ddba382973c4aa1
alinawaz04 <alihnawaz@gmail.com>
2024-05-24 16:04:54 -0500

    updating previous commit (updating context state in child) to use reducer

--- blog/src/context/BlogContext.js
+++ blog/src/context/BlogContext.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 
 /* CONTEXT
 - moves info from a parent to some nested child
@@ -7,14 +7,20 @@ import React, { useState } from "react";
 */
 const BlogContext = React.createContext();
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "add_blogpost":
+      return [...state, { title: `Blog Post #${state.length + 1}` }];
+    default:
+      return state;
+  }
+};
+
 export const BlogProvider = ({ children }) => {
-  const [blogPosts, setBlogPosts] = useState([]);
+  const [blogPosts, dispatch] = useReducer(reducer, []);
 
   const addBlogPost = () => {
-    setBlogPosts([
-      ...blogPosts,
-      { title: `Blog Post #${blogPosts.length + 1}` },
-    ]);
+    dispatch({ type: "add_blogpost" });
   };
 
   return (
