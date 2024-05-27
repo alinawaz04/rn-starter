4b825bc9ca9869638cd72ae60c6652440b7b1305
alinawaz04 <alihnawaz@gmail.com>
2024-05-27 17:27:49 -0500

    mongodb setup + connection

--- track-server/src/index.js
+++ track-server/src/index.js
@@ -1,6 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+const mongoUri =
+  "mongodb+srv://alihnawaz:passwordpassword@cluster0.pq56mw4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+mongoose.connect(mongoUri);
+mongoose.connection.on("connected", () => {
+  console.log("connected to mongo instance");
+});
+mongoose.connection.on("error", (err) => {
+  console.error("error connecting to mongo", err);
+});
 
 // function to run when a GET request is made to API
 app.get("/", (req, res) => {
