6feab241305b6c95fcc68dc468229cc601bb0fbb
alinawaz04 <alihnawaz@gmail.com>
2024-05-28 13:11:47 -0500

    creating tracks - backend + section complete

--- track-server/src/index.js
+++ track-server/src/index.js
@@ -1,8 +1,10 @@
 require("./models/User");
+require("./models/Track");
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const authRoutes = require("./routes/authRoutes");
+const trackRoutes = require("./routes/trackRoutes");
 const requireAuth = require("./middlewares/requireAuth");
 
 const app = express();
@@ -10,6 +12,7 @@ const app = express();
 // handle/parse incoming json info
 app.use(bodyParser.json());
 app.use(authRoutes);
+app.use(trackRoutes);
 
 const mongoUri =
   "mongodb+srv://alihnawaz:passwordpassword@cluster0.pq56mw4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
