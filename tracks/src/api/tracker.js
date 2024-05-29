5381a38bce2617dad4d750ba6bcae3f8c4448ccf
alinawaz04 <alihnawaz@gmail.com>
2024-05-29 16:41:09 -0500

    axios setup

---
+++ tracks/src/api/tracker.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+export default axios.create({
+  baseURL: "https://15de-166-113-81-56.ngrok-free.app",
+});
--- tracks/src/context/AuthContext.js
+++ tracks/src/context/AuthContext.js
@@ -7,8 +7,30 @@ const authReducer = (state, action) => {
   }
 };
 
+const signUp = (dispatch) => {
+  return ({ email, password }) => {
+    // make api req to sign up with given email and password
+    // if sign up, modify state, and say we are authenticated
+    // if sign up fails, reflect error message
+  };
+};
+
+const signIn = (dispatch) => {
+  return ({ email, password }) => {
+    // try to sign in
+    // handle success by updating state
+    // handle failure by showing error message
+  };
+};
+
+const signOut = (dispatch) => {
+  return () => {
+    // somehow sign out
+  };
+};
+
 export const { Provider, Context } = createDataContext(
   authReducer,
-  {},
+  { signIn, signOut, signUp },
   { isSignedIn: false }
 );
