b8685bf21dc3e93cf87ca19275150f6f90cf46be
alinawaz04 <alihnawaz@gmail.com>
2024-05-30 11:37:34 -0500

    extracting form logic

--- tracks/src/screens/SignUpScreen.js
+++ tracks/src/screens/SignUpScreen.js
@@ -1,40 +1,28 @@
-import React, { useState, useContext } from "react";
-import { View, StyleSheet } from "react-native";
-import { Text, Input, Button } from "react-native-elements";
-import Spacer from "../components/Spacer";
+import React, { useContext } from "react";
+import { View, StyleSheet, TouchableOpacity } from "react-native";
+import { Text } from "react-native-elements";
 import { Context as AuthContext } from "../context/AuthContext";
+import Spacer from "../components/Spacer";
+import AuthForm from "../components/AuthForm";
 
 const SignUpScreen = ({ navigation, headerShown = false }) => {
   const { state, signUp } = useContext(AuthContext);
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
 
   return (
     <View style={styles.container}>
-      <Spacer>
-        <Text h3>Sign Up for Tracker</Text>
-      </Spacer>
-      <Input
-        label="Email"
-        value={email}
-        onChangeText={setEmail}
-        autoCapitalize="none"
-        autoCorrect={false}
+      <AuthForm
+        headerText="Sign Up For Tracker"
+        errorMessage={state.errorMessage}
+        submitButtonText="Sign Up"
+        onSubmit={signUp}
       />
-      <Input
-        label="Password"
-        value={password}
-        onChangeText={setPassword}
-        autoCapitalize="none"
-        autoCorrect={false}
-        secureTextEntry
-      />
-      {state.errorMessage ? (
-        <Text style={styles.errorMessage}>{state.errorMessage}</Text>
-      ) : null}
-      <Spacer>
-        <Button title="Sign Up" onPress={() => signUp({ email, password })} />
-      </Spacer>
+      <TouchableOpacity onPress={() => navigation.navigate("SignIn")}>
+        <Spacer>
+          <Text style={styles.link}>
+            Already have an account? Sign in instead
+          </Text>
+        </Spacer>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -45,10 +33,9 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     marginBottom: 300,
   },
-  errorMessage: {
-    fontSize: 16,
-    color: "red",
-    marginLeft: 15,
+
+  link: {
+    color: "blue",
   },
 });
 
