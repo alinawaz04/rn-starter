4fc299bc4f29d2e4337402e9757b306a2e7a87c4
alinawaz04 <alihnawaz@gmail.com>
2024-05-30 18:58:21 -0500

    drawing a series of points

--- tracks/src/components/Map.js
+++ tracks/src/components/Map.js
@@ -1,8 +1,15 @@
 import React from "react";
 import { Text, View, StyleSheet } from "react-native";
-import MapView from "react-native-maps";
+import MapView, { Polyline } from "react-native-maps";
 
 const Map = () => {
+  let points = [];
+  for (let i = 0; i < 20; i++) {
+    points.push({
+      latitude: 35.223586220217996 + i * 0.001,
+      longitude: -97.51090820002473 + i * 0.01,
+    });
+  }
   return (
     <MapView
       style={styles.map}
@@ -12,7 +19,9 @@ const Map = () => {
         latitudeDelta: 0.01,
         longitudeDelta: 0.01,
       }}
-    />
+    >
+      <Polyline coordinates={points} />
+    </MapView>
   );
 };
 
