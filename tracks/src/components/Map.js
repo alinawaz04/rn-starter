aab48c531b2281b153cbf5fbf8a1712ff3af4b52
alinawaz04 <alihnawaz@gmail.com>
2024-06-03 13:29:37 -0500

    live location updates

--- tracks/src/components/Map.js
+++ tracks/src/components/Map.js
@@ -1,27 +1,30 @@
-import React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import React, { useContext } from "react";
+import { ActivityIndicator, StyleSheet } from "react-native";
 import MapView, { Polyline } from "react-native-maps";
+import { Context as LocationContext } from "../context/LocationContext";
 
 const Map = () => {
-  let points = [];
-  for (let i = 0; i < 20; i++) {
-    points.push({
-      latitude: 35.223586220217996 + i * 0.001,
-      longitude: -97.51090820002473 + i * 0.01,
-    });
+  const {
+    state: { currentLocation },
+  } = useContext(LocationContext);
+  if (!currentLocation) {
+    return <ActivityIndicator size="large" style={{ marginTop: 200 }} />;
   }
+
   return (
     <MapView
       style={styles.map}
       initialRegion={{
-        latitude: 35.223586220217996,
-        longitude: -97.51090820002473,
+        ...currentLocation.coords,
         latitudeDelta: 0.01,
         longitudeDelta: 0.01,
       }}
-    >
-      <Polyline coordinates={points} />
-    </MapView>
+      region={{
+        ...currentLocation.coords,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01,
+      }}
+    ></MapView>
   );
 };
 
--- tracks/src/screens/TrackCreateScreen.js
+++ tracks/src/screens/TrackCreateScreen.js
@@ -1,5 +1,5 @@
 // import "../_mockLocation";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import { StyleSheet } from "react-native";
 import { SafeAreaView } from "react-navigation";
 import Map from "../components/Map";
@@ -9,8 +9,10 @@ import {
   watchPositionAsync,
   Accuracy,
 } from "expo-location";
+import { Context as LocationContext } from "../context/LocationContext";
 
 const TrackCreateScreen = () => {
+  const { addLocation } = useContext(LocationContext);
   const [err, setErr] = useState(null);
 
   const startWatching = async () => {
@@ -26,7 +28,7 @@ const TrackCreateScreen = () => {
           distanceInterval: 10,
         },
         (location) => {
-          console.log(location);
+          addLocation(location);
         }
       );
     } catch (e) {
