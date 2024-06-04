0b08e21295d818256f3444be0224649ca1ed711b
alinawaz04 <alihnawaz@gmail.com>
2024-06-04 11:28:39 -0500

    listing all tracks

--- tracks/src/screens/TrackListScreen.js
+++ tracks/src/screens/TrackListScreen.js
@@ -1,13 +1,31 @@
-import React from "react";
-import { Text, View, StyleSheet, Button } from "react-native";
+import React, { useContext } from "react";
+import { Text, StyleSheet, FlatList, TouchableOpacity } from "react-native";
+import { NavigationEvents } from "react-navigation";
+import { ListItem } from "react-native-elements";
+import { Context as TrackContext } from "../context/TrackContext";
 
 const TrackListScreen = ({ navigation }) => {
+  const { state, fetchTracks } = useContext(TrackContext);
+
   return (
     <>
-      <Text>TrackListScreen</Text>
-      <Button
-        title="Go to Track Detail"
-        onPress={() => navigation.navigate("TrackDetail")}
+      <NavigationEvents onWillFocus={fetchTracks} />
+      <Text style={{ fontSize: 48 }}>TrackListScreen</Text>
+      <FlatList
+        data={state}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => {
+          return (
+            <TouchableOpacity>
+              <ListItem>
+                <ListItem.Content>
+                  <ListItem.Title>{item.name}</ListItem.Title>
+                </ListItem.Content>
+                <ListItem.Chevron />
+              </ListItem>
+            </TouchableOpacity>
+          );
+        }}
       />
     </>
   );
@@ -16,3 +34,22 @@ const TrackListScreen = ({ navigation }) => {
 const styles = StyleSheet.create({});
 
 export default TrackListScreen;
+
+{
+  /* <FlatList
+data={state}
+keyExtractor={(item) => item._id}
+renderItem={({ item }) => {
+  return (
+    <TouchableOpacity>
+      <ListItem>
+        <ListItem.Content>
+          <ListItem.Title>{item.name}</ListItem.Title>
+        </ListItem.Content>
+        <ListItem.Chevron />
+      </ListItem>
+    </TouchableOpacity>
+  );
+}}
+/> */
+}
