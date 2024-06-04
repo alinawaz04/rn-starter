6c578e736afb4a49065134a90984ff5ba5fecbf3
alinawaz04 <alihnawaz@gmail.com>
2024-06-04 11:14:33 -0500

    saving track to db and resetting form + navigating

--- tracks/src/context/LocationContext.js
+++ tracks/src/context/LocationContext.js
@@ -17,6 +17,8 @@ const locationReducer = (state, action) => {
       return { ...state, locations: [...state.locations, action.payload] };
     case "change_name":
       return { ...state, name: action.payload };
+    case "reset":
+      return { ...state, name: "", locations: [] };
     default:
       return state;
   }
@@ -54,8 +56,12 @@ const addLocation = (dispatch) => (location, recording) => {
   }
 };
 
+const reset = (dispatch) => () => {
+  dispatch({ type: "reset" });
+};
+
 export const { Context, Provider } = createDataContext(
   locationReducer,
-  { startRecording, stopRecording, addLocation, changeName },
+  { startRecording, stopRecording, addLocation, changeName, reset },
   { name: "", recording: false, locations: [], currentLocation: null }
 );
